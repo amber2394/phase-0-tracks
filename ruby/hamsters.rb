94065c8182b94dc37d569576789e49abc2bf72d1
Amber Maillard <amber.maillard23@gmail.com>
2017-07-24 03:05:41 -0400

    Updates variable assignemnts

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -2,19 +2,20 @@ puts "What is the hamster's name?"
 hamsters_name = gets.chomp
 
 puts "What is the volume level? (1-10)"
-volume_level = gets.chomp
-
-def volume_level(x)
-  x
-
-volume_level (1-10)
-end
+volume_level = gets.to_i
 
 puts "What is the hamster's fur color?"
 fur_color = gets.chomp
 
-puts "Is the hamster a good candidate for adoption?"
+puts "Is the hamster a good candidate for adoption? (y/n)"
 good_candidate = gets.chomp
 
 puts "What is the estimated age of the hamster?"
-age = nil
+age = gets.to_i
+if age == ""
+  age = nil
+end
+
+
+
+
