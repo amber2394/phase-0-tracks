0cc567fe9e7aaa2e22cfe0ec338d0f24f13fbaa8
Amber Maillard <amber.maillard23@gmail.com>
2017-07-24 01:23:47 -0400

    Adds questions and defined variables

---
+++ ruby/hamsters.rb
@@ -0,0 +1,14 @@
+puts "What is the hamster's name?"
+hamsters_name = gets.chomp
+
+puts "What is the volume level? (1-10)"
+volume_level = gets.chomp
+
+puts "What is the hamster's fur color?"
+fur_color = gets.chomp
+
+puts "Is the hamster a good candidate for adoption?"
+good_candidate = gets.chomp
+
+puts "What is the estimated age of the hamster?"
+age = nil
