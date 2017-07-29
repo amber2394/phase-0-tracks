7d1d2e3e2396c052dd8600834e5c2c12a759e0fe
Amber Maillard <amber.maillard23@gmail.com>
2017-07-29 00:53:03 -0400

    Add nested data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,40 @@
+fashion_show = {
+    marc_jacobs: {
+        designer_name: 'Marc Jacobs',
+        seating_info: {
+            front_row_seats: 12,
+            front_row_seats_available: 4
+        },
+        models: [
+            "Kristen Lewis",
+            "Rebecca Salem",
+            "Manuel Lopez",
+            "Gigi Griffins",
+            "Kat Scott"
+        ]
+    },
+    louis_vuiton: {
+        designer_name: 'Louis Vuiton',
+        seating_info: {
+            front_row_seats: 24,
+            fron_row_seats_available: 2
+        },
+        models: [
+            "Lana Clark",
+            "Mischa Buldorf",
+            "Cameron Pitts",
+            "Amber Klein",
+            "Stephen Winshire"
+        ]
+    },
+    calvin_klein: {
+        designer_name: 'Calvin Klein',
+        seating_info: {
+            front_row_seats: 22,
+            front_row_seats_available: 23
+        },
+        models: [
+            "Carol Lauts"
+        ]
+    }
+}
