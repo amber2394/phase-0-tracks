4125d41814c725dbd181e8f675643b8971050f21
Amber Maillard <amber.maillard23@gmail.com>
2017-07-29 02:43:16 -0400

    Add explored documentation of array and hash methods

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -66,3 +66,57 @@ actors.map do |actor, movie|
   movie.upcase
 end
 =end
+
+#.delete_if for arrays
+numbers = [1, 2, 3, 4, 5]
+
+puts "Original data:"
+p numbers
+
+puts "After .delete_if call:"
+numbers.delete_if do |number|
+  number < 3
+end
+
+#.keep_if for arrays
+numbers = [1, 2, 3, 4, 5]
+
+puts "Original data:"
+p numbers
+
+puts "After .keep_if call:"
+numbers.keep_if do |number|
+  number < 3
+end
+
+#.select for arrays
+numbers = [1, 2, 3, 4, 5]
+
+puts "Original data:"
+p numbers
+
+puts "After .select call:"
+numbers.select do |number|
+  number.even?
+end
+
+#.drop_while for arrays
+numbers = [1, 2, 3, 4, 5]
+
+puts "Original data:"
+p numbers
+
+puts "After .drop_while call:"
+numbers.drop_while do |number|
+  number < 3
+end
+
+#.select for hashes
+numbers = {1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five"}
+
+puts "Original data:"
+p numbers
+
+numbers.select do |digit, word|
+  puts digit.even?
+end
