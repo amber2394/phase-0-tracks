f07cd1ab764cbc22c32d1b9b44fe2280d8a2d752
Amber Maillard <amber.maillard23@gmail.com>
2017-07-28 22:40:49 -0400

    Add pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,15 @@
+=begin
+
+Pseudocode:
+
+1. Ask the client their name and recieve input (string)
+2. Ask the client their age and recieve input (integer)
+3. Ask the client if they have children (boolean)
+4. If "yes" ask the client how many children they have
+5. If "no" move on to the next prompt
+6. Ask client to choose a design theme (string)
+7. Print out existing client data and ask the client if this information is correct
+8. If "no" then allow a client to update a key
+9. If "yes" then print "You have succesfully submitted you interior design application!"
+
+=end
