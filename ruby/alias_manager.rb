06db5557994f93875456b6827c063baaf670defd
Amber Maillard <amber.maillard23@gmail.com>
2017-07-29 23:06:50 -0400

    Add user interface

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -13,22 +13,50 @@
   - For 6 & 7 maybe using some combination of .next and index?
 =end
 
-  def vowel_adv(str)
-    puts "What is your name?"
-    name = gets.chomp.downcase.reverse
-    vowels = ["a", "e", "i", "o", "u"]
-    cons = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
-    str = name.split('')
-    str_new = str.map! do |char|
-      if vowels.include?(char)
-        vowels.rotate(1)[vowels.index(char)]
-      elsif cons.include?(char)
-       cons.rotate(1)[cons.index(char)]
-      else
-        char
-      end
+    def vowel_adv(str)
+      puts "What is your name?"
+      name = gets.chomp.downcase.reverse
+      vowels = ["a", "e", "i", "o", "u"]
+      cons = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+      str = name.split('')
+      str_new = str.map! do |char|
+        if vowels.include?(char)
+          vowels.rotate(1)[vowels.index(char)]
+        elsif cons.include?(char)
+         cons.rotate(1)[cons.index(char)]
+        else
+          char
+        end
+    end
+  puts "Your secret agent name is:"
+  str_new.join.reverse.split.map(&:capitalize).reverse.join(' ')
   end
-puts "Your secret agent name is:"
-str_new.join.reverse.split.map(&:capitalize).reverse.join(' ')
+  puts vowel_adv "aeiou"
+loop do
+  puts "Are you happy with your secret agent name? (y/n)"
+   user_input = gets.chomp
+  if user_input == "n"
+       def vowel_adv(str)
+        puts "What is your name?"
+        name = gets.chomp.downcase.reverse
+        vowels = ["a", "e", "i", "o", "u"]
+        cons = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+        str = name.split('')
+        str_new = str.map! do |char|
+          if vowels.include?(char)
+          vowels.rotate(1)[vowels.index(char)]
+          elsif cons.include?(char)
+         cons.rotate(1)[cons.index(char)]
+          else
+            char
+          end
+        end
+    puts "Your secret agent name is:"
+    str_new.join.reverse.split.map(&:capitalize).reverse.join(' ')
+        end
+    puts vowel_adv "aeiou"
+  else
+    break if user_input == "y"
+  end
 end
-puts vowel_adv "aeiou"
+puts "Congratulations on your new secrent agent name!"
