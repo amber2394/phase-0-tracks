cb7b54f416d933c73e882b510f012a2cc7c809ed
Amber Maillard <amber.maillard23@gmail.com>
2017-07-29 20:46:14 -0400

    Add pseudocode

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,14 @@
+=begin
+  Pseudocode:
+1. Establish the first and last name
+  - string
+2. Use the .split method to separate "Felicia Torres" to "Felicia" "Torres"
+3. Reverse the first and last name to create "Torres" "Felicia".
+  - .reverse
+4. Join the first and last name together to create "Torres Felicia"
+  -.join
+5. Split each character to create an array.
+6. Employ a method to move vowels to the next letter of vowels
+7. Employ a method to move each constenant to the next consenant
+  - For 6 & 7 maybe using some combination of .next and index?
+=end
