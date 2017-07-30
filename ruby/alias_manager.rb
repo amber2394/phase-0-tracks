58a2fa314e1eb05a4d0196c6a31e62f943f76c53
Amber Maillard <amber.maillard23@gmail.com>
2017-07-30 01:01:34 -0400

    Add commented out data structure

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -60,3 +60,66 @@ loop do
   end
 end
 puts "Congratulations on your new secrent agent name!"
+
+=begin
+
+I cant seem to figure out how to implement the data structure properly... This is what I have so far:
+
+fake_names = {}
+
+    def vowel_adv(str)
+      puts "What is your name?"
+      name = gets.chomp.downcase.reverse
+      fake_names = {}
+      fake_names[:real_name] = name
+      vowels = ["a", "e", "i", "o", "u"]
+      cons = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+      str = name.split('')
+      str_new = str.map! do |char|
+        if vowels.include?(char)
+          vowels.rotate(1)[vowels.index(char)]
+        elsif cons.include?(char)
+         cons.rotate(1)[cons.index(char)]
+        else
+          char
+        end
+    end
+  puts "Your secret agent name is:"
+  p = str_new.join.reverse.split.map(&:capitalize).reverse.join(' ')
+  name = str_new.join.reverse.split.map(&:capitalize).reverse.join(' ')
+  fake_names = {}
+  fake_names[:fake_name] = name
+  end
+  puts vowel_adv "aeiou"
+loop do
+  puts "Are you happy with your secret agent name? (y/n)"
+   user_input = gets.chomp
+  if user_input == "n"
+       def vowel_adv(str)
+        puts "What is your name?"
+        name = gets.chomp.downcase.reverse
+        vowels = ["a", "e", "i", "o", "u"]
+        cons = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+        str = name.split('')
+        str_new = str.map! do |char|
+          if vowels.include?(char)
+          vowels.rotate(1)[vowels.index(char)]
+          elsif cons.include?(char)
+         cons.rotate(1)[cons.index(char)]
+          else
+            char
+          end
+        end
+    puts "Your secret agent name is:"
+    str_new.join.reverse.split.map(&:capitalize).reverse.join(' ')
+    fake_names[:fake_name] = str_new.join.reverse.split.map(&:capitalize).reverse.join(' ')
+        end
+    puts vowel_adv "aeiou"
+  else
+    break if user_input == "y"
+  end
+end
+puts "Congratulations on your new secret agent name!"
+p fake_names
+
+=end
