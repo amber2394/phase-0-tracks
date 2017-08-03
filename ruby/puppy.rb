4f4922d451615d442f6f14448e9a139ab904393c
Amber Maillard <amber.maillard23@gmail.com>
2017-08-02 21:43:14 -0400

    Add puppy class design

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+=begin
+SPECIES ------------------------
+German Shephard
+
+CHARACTERISTICS ----------------
+Fur: Yes
+Friendly: yes
+Eye count: 2
+Leg count: 4
+Color: varies
+
+BEHAVIOR -----------------------
+Jump
+Bark
+Whine
+Growl
+=end
+
