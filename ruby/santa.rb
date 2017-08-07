477b6a51c2182fce94dbc4c12a1e6c973d38864c
Amber Maillard <amber.maillard23@gmail.com>
2017-08-07 03:25:34 -0400

    Add attribute changing methods and getter methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,13 @@
 class Santa
 
+  def initialize(gender, ethnicity)
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
+    puts "Initializing Santa instance ..."
+  end
+
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
   end
@@ -8,19 +16,34 @@ class Santa
     puts "That was a good #{cookie}!"
   end
 
-  def initialize(gender, ethnicity)
-    @gender = gender
-    @ethnicity = ethnicity
-    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-    @age = 0
-    puts "Initializing Santa instance ..."
+  def celebrate_birthday
+    @age += 1
+  end
 
+  def get_mad_at
+    puts "#{@reindeer_ranking.rotate(1)}"
+  end
+
+  def new_gender=(gender)
+    @gender = gender
+  end
+
+  def age
+    @age
+   end
+
+  def ethnicity
+    @ethnicity
   end
 end
 
-#nicholas = Santa.new
-#nicholas.speak
-#nicholas.eat_milk_and_cookies("snickerdoodle")
+
+
+nicholas = Santa.new("female", "black")
+nicholas.speak
+nicholas.eat_milk_and_cookies("snickerdoodle")
+nicholas.age
+nicholas.ethnicity
 
 =begin
 santas = []
@@ -33,9 +56,11 @@ santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
 santas << Santa.new("N/A", "N/A")
 =end
 
+=begin
 santas = []
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
 example_genders.length.times do |i|
   santas << Santa.new(example_genders[i], example_ethnicities[i])
 end
+=end
