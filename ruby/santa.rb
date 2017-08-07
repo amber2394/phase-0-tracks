c0770225802075e8f4cfd2785b9a39c3b7343f9e
Amber Maillard <amber.maillard23@gmail.com>
2017-08-07 02:56:10 -0400

    Add attributes and updates driver code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -8,11 +8,34 @@ class Santa
     puts "That was a good #{cookie}!"
   end
 
-  def initialize
+  def initialize(gender, ethnicity)
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
     puts "Initializing Santa instance ..."
+
   end
 end
 
-nicholas = Santa.new
-nicholas.speak
-nicholas.eat_milk_and_cookies("snickerdoodle")
+#nicholas = Santa.new
+#nicholas.speak
+#nicholas.eat_milk_and_cookies("snickerdoodle")
+
+=begin
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
+=end
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
