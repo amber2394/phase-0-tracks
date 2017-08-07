8b21cfd7e4f6f2ed3935a499d1f4539934c77d49
Amber Maillard <amber.maillard23@gmail.com>
2017-08-07 05:03:03 -0400

    Add class to satisfy tests

--- ruby/list/todo_list_spec.rb
+++ ruby/list/todo_list_spec.rb
@@ -1,5 +1,7 @@
-describe TodoList do
-  let(:list) { TodoList.new(["do the dishes", "mow the lawn"]) }
+require_relative 'todo_list'
+
+describe Todo_list do
+  let(:list) { Todo_list.new(["do the dishes", "mow the lawn"]) }
 
   it "stores the list items given on initialization" do
     expect(list.get_items).to eq ["do the dishes", "mow the lawn"]
