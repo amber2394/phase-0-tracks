a383f24ec5e0de795d548ca22b480db4f54b47fc
Amber Maillard <amber.maillard23@gmail.com>
2017-08-12 02:37:19 -0400

    add pseudocode for javascript exploration

---
+++ js/explore.js
@@ -0,0 +1,6 @@
+// Write a function that takes a string parameter
+// a) Function will be called reverse and take a parameter of str
+// b) Set a variable called a equal to '';
+// c) Set a variable i equal to the length of str with the condition if i is greater than or equal to 0, i will count backwords using i--
+// Add variable a to str with the condition of i
+// Return variable a
