8dedfa7f4ee99c7fa252f099e0891d2c56f6458b
Amber Maillard <amber.maillard23@gmail.com>
2017-08-14 01:39:16 -0400

    Add pseudocode for Release 0

--- js/algos.js
+++ js/algos.js
@@ -1,3 +1,12 @@
+// Create an array 'phrases' with a variety of phrases in different lengths
+// Define a variable lgth set to the value 0
+// Define an empty variable longest to be used in the loop
+// Input 1: While i (set to 0) is less than the length of the phrases, iterate through the array by 1
+// Input 2: (Conditionals) If the length of the indexed phrase's length is less than lgth (set to 0), reassign lgth to the length of the phrases
+// Input 3: Set var longest equal to the indexed phrases
+// Output: Print longest
+
+
 var phrases = ["long phrase","longest phrase","longer phrase"];
 var lgth = 0;
 var longest;
