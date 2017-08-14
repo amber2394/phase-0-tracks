a57f4430a89e9456d63fcb128e7aa8eba53c1db5
Amber Maillard <amber.maillard23@gmail.com>
2017-08-14 00:56:42 -0400

    Add code to assign horse names to color to form an object

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,5 +1,15 @@
 var horse_colors = ["Blue", "Pink", "Yellow", "Purple"];
 var horse_names = ["Ed", "Lola", "Tilly", "Waffle"];
+var complete_horse = {};
 
 horse_colors.push("Red");
 horse_names.push("Moxxy");
+
+function horse_completer(names, colors) {
+for (var i = horse_names.length - 1; i >= 0; i--) {
+  complete_horse[names[i]] = colors[i]
+  }
+  console.log(complete_horse);
+}
+
+horse_completer(horse_names, horse_colors)
