bcd0f73dc7c9d5dd694e0d0574c4454e803fc66c
Amber Maillard <amber.maillard23@gmail.com>
2017-08-14 02:12:01 -0400

    Add a function that takes two objects and checks to see if the objects share at least one key-value pairr

--- js/algos.js
+++ js/algos.js
@@ -21,3 +21,34 @@ for(var i=0; i < phrases.length; i++){
 }
 
 console.log(longest);
+
+// For the life of me, I couldn't get this one to work using objects, only arrays. However, I did manage to find a piece of code only that made this run with objects. (Even though, it only seems to return false, so something is still wrong) I attempt to explain what is happening in this code to demonstrate that I know what is being done in my pseudocode:
+
+// Set up 3 objects with 3 key/value pairs named students_a, students_b, students_c
+// Create a function that compares two keys and values between the objects.
+// Input 1: checkIfObjectContains has two parameters one and two which stand in for one = keys, two = values
+// **This is where I become lost a little**
+// Return false if either condition is met
+// Return true if those conditions are not met
+// Print test results of varying object pairs
+
+var students_a = {"Melissa" : "A+", "Daniel": "B-", "Kyle" : "A"};
+var students_b = {"Lauren" : "B+", "Lilith": "A", "Kelly": "C"};
+var students_c = {"Lauren" : "B+", "Melissa" : "A+", "Chester" : "D"};
+
+function checkIfObjectContains(one, two){
+   for (var i in one) {
+           if (! two.hasOwnProperty(i) || one[i] !== two[i] ) {
+              return false;
+           }
+   }
+   return true;
+}
+
+console.log("Comparison results" + ": " + checkIfObjectContains(students_a, students_b));
+console.log("Comparison results" + ": " + checkIfObjectContains(students_b, students_c));
+console.log("Comparison results" + ": " + checkIfObjectContains(students_c, students_b));
+console.log("Comparison results" + ": " + checkIfObjectContains(students_c, students_a));
+
+
+
